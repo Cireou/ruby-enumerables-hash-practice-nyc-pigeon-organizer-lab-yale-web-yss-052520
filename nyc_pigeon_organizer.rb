c3bd074ec8a2f86c8e116856fbecf5563d3febbb
Eric  Ou <eric.ou@yale.edu>
2020-05-07 05:17:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,11 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each {|key,val| 
+  data.each {|key,val| collect_attr(new_hash, key, val)}
+  new_hash
 end
+
+def collect_attr(new_hash, key, val)
+  
   
   
   
