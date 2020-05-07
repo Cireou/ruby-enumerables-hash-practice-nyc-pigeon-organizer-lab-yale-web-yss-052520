c035fc4533c57386f421ea7a492916667f166065
Eric  Ou <eric.ou@yale.edu>
2020-05-07 05:07:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,8 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  gather_names(new_hash, data)
+  gather_names(new_hash, data.first)
 end
 
-def gather_names(new_hash, data)
+def gather_names(new_hash, data.first) 
+# Assume that the first key-val pair contains all the pigeons 
   
